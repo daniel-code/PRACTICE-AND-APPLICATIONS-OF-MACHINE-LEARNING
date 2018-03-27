e9180909b2ec124a21e3cdcfe8ba4fd40d5c87bf
daniel-code <daniel78761215@gmail.com>
2018-03-27 14:10:13 +0800

    更改feature數量

--- exercise2/plot_iris/plot_iris1.py
+++ exercise2/plot_iris/plot_iris1.py
@@ -28,6 +28,7 @@ plot_step = 0.02
 # Load data
 iris = load_iris()
 
+#use two feature by pair
 for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3],
                                 [1, 2], [1, 3], [2, 3]]):
     # We only take the two corresponding features
@@ -76,3 +77,66 @@ for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3],
 plt.suptitle("Decision surface of a decision tree using paired features")
 plt.legend()
 plt.show()
+
+#use only one feature
+for pairidx, pair in enumerate([[0, 0], [1, 1], [2, 2],
+                                [3, 3]]):
+    # We only take the two corresponding features
+    X = iris.data[:, pair]
+    y = iris.target
+
+    # Shuffle
+    idx = np.arange(X.shape[0])
+    np.random.seed(13)
+    np.random.shuffle(idx)
+    X = X[idx]
+    y = y[idx]
+
+    # Standardize
+    mean = X.mean(axis=0)
+    std = X.std(axis=0)
+    X = (X - mean) / std
+
+    # Train
+    clf = DecisionTreeClassifier().fit(X, y)
+
+    # Plot the decision boundary
+    plt.subplot(2, 3, pairidx + 1)
+
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                         np.arange(y_min, y_max, plot_step))
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+
+    plt.xlabel(iris.feature_names[pair[0]])
+    plt.ylabel(iris.feature_names[pair[1]])
+    plt.axis("tight")
+
+    # Plot the training points
+    for i, color in zip(range(n_classes), plot_colors):
+        idx = np.where(y == i)
+        plt.scatter(X[idx, 0], X[idx, 1], c=color, label=iris.target_names[i],
+                    cmap=plt.cm.Paired)
+
+    plt.axis("tight")
+
+plt.suptitle("Decision surface of a decision tree using paired features")
+plt.legend()
+plt.show()
+
+
+#full feature
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(X=X_train,y=y_train)
+Z = clf.predict(X=X_test)
+print(classification_report(y_test, Z, target_names=iris.feature_names))
