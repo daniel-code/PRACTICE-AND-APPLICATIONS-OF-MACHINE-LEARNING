0f043f755e0782e8c3cf1849e2e8340b94604c70
daniel-code <daniel78761215@gmail.com>
2018-04-02 16:42:22 +0800

    更改線性模型
    新繪製全部股價圖片

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -9,7 +9,7 @@ Date:2018.04.02
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 # 參數設定
@@ -27,13 +27,21 @@ print(y)
 x_train, x_test = x[:-TEST_NUM], x[-TEST_NUM:]
 y_train, y_test = y[:-TEST_NUM], y[-TEST_NUM:]
 # 訓練
-model = MLPRegressor()
+model = LinearRegression()
 model.fit(X=x_train, y=y_train)
 # 預測
 predict = model.predict(x_test)
 print('Score = ', model.score(x_test, y_test))
-# 繪製結果
+
+#繪製全部價錢
 plt.title(STOCK_FILE + ' stock price')
+plt.xlabel('days')
+plt.ylabel('price')
+plt.plot(y)
+plt.show()
+
+# 繪製結果
+plt.title(STOCK_FILE + ' stock price prediction')
 plt.xlabel('days')
 plt.ylabel('price')
 plt.plot(y_test,label='real')
