965e942d00c2160c3a5c7c2d4d3058d154df91f7
daniel-code <daniel78761215@gmail.com>
2018-04-03 00:30:13 +0800

    更改預測對象
    新增參考對象

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -13,17 +13,21 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 # 參數設定
-STOCK_FILE = 'Apple'
-TEST_NUM = 100
+STOCK_FILE = 'Foxconn'
+REF_FILE = 'Apple'
+TEST_NUM = 1000
 # 讀取資料
 data = pd.read_csv(STOCK_FILE + '.csv')
-data_list = data.values
+reference = pd.read_csv(REF_FILE + '.csv')
+
+data['Time'] = pd.to_datetime(data['Time'], format='%Y/%m/%d')
+reference['Time'] = pd.to_datetime(reference['Time'], format='%Y/%m/%d')
+
+data_list = data[(data['Time'] > '1991-06-18')].values
+reference_list = reference[(reference['Time'] > '1991-06-18')].values
 
 x = data_list[:-1, 1:4]
 y = data_list[1:, 4]
-print(data_list)
-print(x)
-print(y)
 x_train, x_test = x[:-TEST_NUM], x[-TEST_NUM:]
 y_train, y_test = y[:-TEST_NUM], y[-TEST_NUM:]
 # 訓練
@@ -33,7 +37,7 @@ model.fit(X=x_train, y=y_train)
 predict = model.predict(x_test)
 print('Score = ', model.score(x_test, y_test))
 
-#繪製全部價錢
+# 繪製全部價錢
 plt.title(STOCK_FILE + ' stock price')
 plt.xlabel('days')
 plt.ylabel('price')
@@ -44,7 +48,7 @@ plt.show()
 plt.title(STOCK_FILE + ' stock price prediction')
 plt.xlabel('days')
 plt.ylabel('price')
-plt.plot(y_test,label='real')
-plt.plot(predict,label='predict')
+plt.plot(y_test, label='real')
+plt.plot(predict, label='predict')
 plt.legend(loc='upper left')
 plt.show()
